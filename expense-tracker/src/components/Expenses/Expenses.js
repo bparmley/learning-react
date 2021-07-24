571ce0ae75f3dbd1b31149fb0a808663888e96ab
bparmley <orem.bparmley@gmail.com>
2021-07-24 14:35:30 -0600

    Modularized conditial for filtered results

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2021");
@@ -17,26 +17,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found. Try changing your filters.</p>
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
