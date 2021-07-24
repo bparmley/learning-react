7d3907c2d1996f0dc997d8bf537c66b18384a3af
bparmley <orem.bparmley@gmail.com>
2021-07-24 13:26:06 -0600

    Add ID to expenses map

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,36 +1,41 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Quest Bars",
+    amount: 34.62,
+    date: new Date(2021, 3, 3),
+  },
+  {
+    id: "e2",
+    title: "Quest Bars",
+    amount: 34.62,
+    date: new Date(2021, 4, 3),
+  },
+  {
+    id: "e3",
+    title: "Quest Bars",
+    amount: 34.62,
+    date: new Date(2021, 5, 3),
+  },
+  {
+    id: "e4",
+    title: "Quest Bars",
+    amount: 34.62,
+    date: new Date(2021, 6, 3),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Quest Bars",
-      amount: 34.62,
-      date: new Date(2021, 3, 3),
-    },
-    {
-      id: "e2",
-      title: "Quest Bars",
-      amount: 34.62,
-      date: new Date(2021, 4, 3),
-    },
-    {
-      id: "e3",
-      title: "Quest Bars",
-      amount: 34.62,
-      date: new Date(2021, 5, 3),
-    },
-    {
-      id: "e4",
-      title: "Quest Bars",
-      amount: 34.62,
-      date: new Date(2021, 6, 3),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(expenses);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
