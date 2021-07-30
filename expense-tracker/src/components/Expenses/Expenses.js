00dafb11415413fb20628a34004df22124a95fff
bparmley <orem.bparmley@gmail.com>
2021-07-30 08:07:02 -0600

    Updated code to match the udemy course, mainly updating naming conventions

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -1,30 +1,31 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import ExpensesFilter from "./ExpensesFilter";
-import "./Expenses.css";
-import Card from "../UI/Card";
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2021");
-  const expenses = props.expenses;
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = expenses.filter((expense) => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
-      />
-      <ExpensesList items={filteredExpenses} />
-    </Card>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
   );
 };
 
