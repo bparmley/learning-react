149d2a6002074c93d05f110527aee956c921df5b
bparmley <orem.bparmley@gmail.com>
2021-07-30 08:17:11 -0600

    Fixed an error.

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,31 +1,27 @@
-import React, { useState } from "react";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import React, { useState } from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 const DUMMY_EXPENSES = [
   {
-    id: "e1",
-    title: "Quest Bars",
-    amount: 34.62,
-    date: new Date(2021, 3, 3),
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
   },
   {
-    id: "e2",
-    title: "Quest Bars",
-    amount: 34.62,
-    date: new Date(2021, 4, 3),
-  },
-  {
-    id: "e3",
-    title: "Quest Bars",
-    amount: 34.62,
-    date: new Date(2021, 5, 3),
-  },
-  {
-    id: "e4",
-    title: "Quest Bars",
-    amount: 34.62,
-    date: new Date(2021, 6, 3),
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
   },
 ];
 
@@ -33,15 +29,22 @@ const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    setExpenses(prevExpenses => {
+    setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
   };
 
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
